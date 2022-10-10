efa00b98cdd58b88fefd8955f9dc9a28a56b2239
Sanjayvadla <112846438+Sanjayvadla@users.noreply.github.com>
2022-10-10 09:45:31 +0530

    day6 q1

---
+++ day6 q1.py
@@ -0,0 +1,10 @@
+n=int(input("X="))
+if(n>0):
+    for i in range(1,n):
+        for j in range(i+1,n):
+            for k in range(j+1,n):
+                if(i**2+j**2==k**2):
+                    print(i,j,k)
+else:
+    print("enter the value greater than 0")
+                
